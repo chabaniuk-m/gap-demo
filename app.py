f800d9b1927b18e403408a829547a3c8767ab607
Oleh <tsipotan.oleh@gmail.com>
2022-04-29 11:39:33 +0300

    .idea folder ignored

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def main_page():
+@app.route("/", methods=['GET'])
+def main_page_get():
     return render_template("main_page.html")
 
 
-@app.route("/solution_test", methods=['GET'])
-def solution_test():
-    return render_template("solution_test.html")
+@app.route("/solution_test", methods=['GET', 'POST'])
+def solution_test_GET():
+    if request.method == 'POST':
+        print(request.form.get('polynom'))
+    return render_template('solution_test.html')
+
