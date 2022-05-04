2deed7d65510e63408c5cac18e77c4c8b7bf698b
Artem Poliakov <artem4250@gmail.com>
2022-05-04 13:20:33 +0300

    TEST(Polynom, division)

--- Polynom_tests.cpp
+++ Polynom_tests.cpp
@@ -4,7 +4,26 @@
 
 int mod;
 
-TEST(Polynom, operatorPlus)
+TEST(Polynom, division)
 {
-    EXPECT_EQ(2+2,4);
+    Polynom a, b;
+    std::pair<Polynom,Polynom> res;
+
+    mod = 5;
+    a = {new int[5]{2,0,1,3,4},5};
+    b = {new int[3]{1,3,3},3};
+    res = {Polynom(new int[3]{3,3,3},3), Polynom(new int[2]{4,3},2)};
+    EXPECT_EQ(Polynom::division(a,b), res);
+
+    mod = 3;
+    a = {new int[5]{2,0,1,3,0},5};
+    b = {new int[3]{1,0,3},3};
+    res = {Polynom(new int[3]{2,0,1},3), Polynom()};
+    EXPECT_EQ(Polynom::division(a,b), res);
+
+    mod = 7;
+    a = {new int[7]{6,0,1,3,6,0,0},7};
+    b = {new int[6]{3,1,3,0,5,0},6};
+    res = {Polynom(new int[1]{4},1), Polynom(new int[4]{1,3,3,3},4)};
+    EXPECT_EQ(Polynom::division(a,b), res);
 }
