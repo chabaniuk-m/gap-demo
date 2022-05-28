127fdffb52ca0c00a1c28c765b701e072179d139
ratatyu4343 <93818812+ratatyu4343@users.noreply.github.com>
2022-05-28 12:38:41 +0300

    template added

--- C-solution/Int/task_10/task_10.cpp
+++ C-solution/Int/task_10/task_10.cpp
@@ -1,1 +1,14 @@
+#pragma once
+#include "../task_4/task_4.cpp"
 
+/*
+Miller Rabin's primacy test
+if n - probably primary - return TRUE
+if n is compounded - return FALSE
+the probability of error is 0.25 ^ t
+int n is the number to check, int t is the confidence level
+* /
+bool miillerTest(int n, int t)
+{
+
+}
