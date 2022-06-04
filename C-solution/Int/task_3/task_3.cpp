ab09fff49435769d3ca4c4cf9ec303d25f464cdb
AlexPal005 <91566487+AlexPal005@users.noreply.github.com>
2022-06-04 20:00:10 +0300

    Update task_3.cpp
    add check co prime

--- C-solution/Int/task_3/task_3.cpp
+++ C-solution/Int/task_3/task_3.cpp
@@ -2,22 +2,49 @@
 #include "..\..\const.h"
 
 using namespace std;
-
+bool is_co_prime(int a, int b);
+// головна функція  (обернений елемент)
 int inverted_element(int value) {
 
     int result = 0;
 
+	if (is_co_prime(mod, value)) {
+		for (int i = 0; i < mod; i++) {
 
-    for (int i = 0; i < mod; i++) {
+			result = (value * i) % mod;
 
-        result = (value * i) % mod;
+			if (result == 1) {
 
-        if (result == 1) {
+				return i;
 
-            return i;
+			}
 
-        }
+		}
+	}
+	else {
+		// -1 - error
+		return -1;
+	}
+	
+}
+//перевірка чи mod і value взаємнопрості
+bool is_co_prime(int a , int b) {
+	int hcf = 0;
+	//найти найбільший спільний дільник
+	for (int i = 1; i <= a; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+		{
+			hcf = i;
+		}
+	}
 
-    }
-
+	if (hcf == 1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
