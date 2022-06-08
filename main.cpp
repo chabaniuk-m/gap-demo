0b9b6a4921a5118f257c157de24e5956caea3235
Artem Poliakov <artem4250@gmail.com>
2022-06-08 11:54:57 +0300

    update

--- main.cpp
+++ main.cpp
@@ -6,11 +6,50 @@ int mod;
 
 int main()
 {
-    Polynom a,b;
+    Polynom a,b, res;
+    std::string p1, p2;
 
-    mod = 23;
-    a = Polynom("6x+2");
-    b = Polynom("2");
 
-    (a/b).print();
+    /*
+    while(true)
+    {
+        std::cout<<"Modulus: ";
+        std::cin>>mod;
+        std::cout<<"Polynomial 1: ";
+        std::cin>>p1;
+        a = Polynom(p1);
+        std::cout<<"Polynomial 2: ";
+        std::cin>>p2;
+        b = Polynom(p2);
+
+        std::cout<<"a/b = ";
+        res = a/b;
+        res.print();
+        std::cout<<std::endl;
+
+        std::cout<<"a%b = ";
+        res = a%b;
+        res.print();
+        std::cout<<std::endl;
+    }
+     */
+
+
+    while(true)
+    {
+        std::cout<<"Modulus: ";
+        std::cin>>mod;
+        std::cout<<"Polynomial 1: ";
+        std::cin>>p1;
+        a = Polynom(p1);
+        std::cout<<"Polynomial 2: ";
+        std::cin>>p2;
+        b = Polynom(p2);
+
+        std::cout<<"GCD(a,b) = ";
+        res = gcd(a,b);
+        res.print();
+        std::cout<<std::endl;
+    }
+
 }
