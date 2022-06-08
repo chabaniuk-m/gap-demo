e54ddcb5b4ce2846eb69891400afae56110d5699
Artem Poliakov <artem4250@gmail.com>
2022-06-08 03:25:08 +0300

    Update task_3.cpp

--- C-solution/Int/task_3/task_3.cpp
+++ C-solution/Int/task_3/task_3.cpp
@@ -6,27 +6,18 @@ using namespace std;
 bool is_co_prime(int a, int b);
 // головна функція  (обернений елемент)
 int inverted_element(int value) {
-
     int result = 0;
-
 	if (is_co_prime(mod, value)) {
 		for (int i = 0; i < mod; i++) {
-
 			result = (value * i) % mod;
-
 			if (result == 1) {
-
 				return i;
-
 			}
-
 		}
 	}
-	else {
-		// -1 - error
-		return -1;
-	}
-	
+
+    // -1 - error
+    return -1;
 }
 //перевірка чи mod і value взаємнопрості
 bool is_co_prime(int a , int b) {
@@ -55,10 +46,5 @@ bool is_co_prime(int a , int b) {
 int divide(int a, int b) {
     if (!number::isPrime(mod))
         return -1;
-    int** divTable = number::generateDivisionTable();
-    a = number::recover(a);
-    b = number::recover(b);
-    int res = divTable[a][b];
-    number::deleteDivisionTable(divTable);
-    return res;
+    return number::recover(a*inverted_element(b));
 }
